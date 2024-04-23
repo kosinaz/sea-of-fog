b020b7ca53db77a15e578fd72627edf49c717b2c
Kosina Zoltán <kosinaz@gmail.com>
2024-04-23 21:29:40 +0200

    add dynamic fog vs shadow effect

--- Sea of Fog/map.gd
+++ Sea of Fog/map.gd
@@ -14,6 +14,14 @@ func _unhandled_input(event):
 	var map_position = $TileMap.world_to_map($"%Player".position)
 	var walls = $TileMap.get_used_cells_by_id(4)
 	var current_fov = fov.calculate(map_position.x, map_position.y, 7, walls)
+	var tile = $"%TileMap".get_cell(map_position.x, map_position.y)
+	if tile == 2:
+		$"%Player/Sprite".modulate = Color(1, 1, 1, 1)
+	else:
+		$"%Player/Sprite".modulate = Color(0, 0, 0, 1)
 	$"%TileMap2".clear()
+	for x in range(-16, 16):
+		for y in range(-8, 8):
+			$"%TileMap2".set_cell(map_position.x + x, map_position.y + y, 5 if tile == 2 else 0)
 	for cell in current_fov:
 		$"%TileMap2".set_cell(cell.x, cell.y, $"%TileMap".get_cell(cell.x, cell.y), false, false, false, $"%TileMap".get_cell_autotile_coord(cell.x, cell.y))
